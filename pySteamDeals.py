d8d1cc9f0e7bbde2869cb9dd918f010d18c2081d
Leandro Toledo <leandrotoledodesouza@gmail.com>
2016-05-07 22:38:51 -0300

    Code refactor

--- pySteamDeals.py
+++ pySteamDeals.py
@@ -1,70 +1,92 @@
-import urllib.request
+import requests
 from bs4 import BeautifulSoup
 from termcolor import colored
-  
+
+BASE_URL = 'http://store.steampowered.com/search/results?sort_by=_ASC&os=win&specials=1'
+CURRENCY_SYMBOL = '€'
+
 
 def get_html(url):
-   req = urllib.request.Request(url, headers={'User_Agent':'Mozilla/5.0'})
-   return urllib.request.urlopen(req)
+   html = requests.get(url, headers={'User_Agent':'Mozilla/5.0'})
+   return html.text
 
 
 def list_titles(url):
-   titles = []
-   soup = BeautifulSoup(get_html(url))
-   for link in soup.findAll('span'):
-      if 'title' in str(link.get('class')):
-         game_title = str(link)
-         titles.append(game_title[20:-7])
+   soup = BeautifulSoup(get_html(url), "html.parser")
+
+   titles = list()
+   for link in soup.find_all('span', attrs={'class': 'title'}):
+         game_title = link.text.strip()
+         titles.append(game_title)
+
    return titles
 
 
 def list_discounts(url):
-   discounts = []
-   soup = BeautifulSoup(get_html(url))
-   for link in soup.findAll('div'):
-      if 'search_discount' in str(link.get('class')):
-         game_discount = str(link)
-         discounts.append(''.join(c for c in game_discount if c in '-%0123456789'))
+   soup = BeautifulSoup(get_html(url), "html.parser")
+
+   discounts = list()
+   for link in soup.find_all('div', attrs={'class': 'search_discount'}):
+         game_discount = link.text.replace('-', '').replace('%', '').strip()
+         game_discount = int(game_discount)
+         discounts.append(game_discount)
+
    return discounts
 
 
 def list_original_prices(url):
-   original_prices = []
-   soup = BeautifulSoup(get_html(url))
-   for link in soup.findAll('strike'):
-      if '€' in str(link):
-         price = str(link)
-         original_prices.append(''.join(c for c in price if c in ',€0123456789'))
+   soup = BeautifulSoup(get_html(url), "html.parser")
+
+   original_prices = list()
+   for link in soup.find_all('strike'):
+      if CURRENCY_SYMBOL in link.text:
+         price = link.text.replace(CURRENCY_SYMBOL, '').replace(',', '.').strip()
+         price = float(price)
+         original_prices.append(price)
+
    return original_prices
 
 
+def list_discounted_prices(url):
+   soup = BeautifulSoup(get_html(url), "html.parser")
+
+   discounted_prices = list()
+   for link in soup.find_all('div', attrs={'class': 'discounted'}):
+      if CURRENCY_SYMBOL in link.text:
+         price = link.text.replace(CURRENCY_SYMBOL, '').replace(',', '.').split(' ')[-1].strip()
+
+         try:
+            price = float(price)
+         except ValueError:
+            if 'Free' in price:
+                price = 0
+
+         discounted_prices.append(price)
+
+   return discounted_prices
+
+
 def get_links():
-   base_url = 'http://store.steampowered.com/search/?specials=1&os=win#sort_by=_ASC&os=win%2Cmac%2Clinux&specials=1&page=1'
-   links = [base_url]
-   soup = BeautifulSoup(get_html(base_url))
-   for link in soup.findAll('a'):
-      if 'SearchLinkClick' in str(link.get('onclick')):
-         href = str(link.get('href'))
-         links.append(href)
-   return links[:-1]
+   soup = BeautifulSoup(get_html(BASE_URL), "html.parser")
 
+   last_page = soup.find_all('a', attrs={'onclick': 'SearchLinkClick( this ); return false;'})[2]
+   last_page = int(last_page.text)
 
-def to_float(d):
-   return float(''.join(c for c in d if c in '0123456789'))  
+   links = list()
+   for i in range(1, last_page+1):
+       link = '{}&page={}'.format(BASE_URL, i)
+       links.append(link)
 
-
-def get_discounted_price(price, discount):
-   return to_float(price)/100.0 - ((to_float(discount)/100.0) * to_float(price))/100.0
-
+   return links
 
 
 for url in get_links():
-   games = zip(list_titles(url), list_discounts(url), list_original_prices(url))
-   for title, discount, o_price in games:
-      if to_float(discount) < 50:
-         print(title, colored(discount, 'blue') + ' from ' + o_price + ' to ' + '%.2f€' % (get_discounted_price(o_price, discount)))
-      elif to_float(discount) > 50 and to_float(discount) < 75:
-         print(title, colored(discount, 'yellow') + ' from ' + o_price + ' to ' + '%.2f€' % (get_discounted_price(o_price, discount)))
-      elif to_float(discount) > 75:
-         print(title, colored(discount, 'green') + ' from ' + o_price + ' to ' + '%.2f€' % (get_discounted_price(o_price, discount)))
+    games = zip(list_titles(url), list_discounts(url), list_original_prices(url), list_discounted_prices(url))
 
+    for title, discount, o_price, n_price in games:
+        if discount < 50:
+            print(title, colored(discount, 'blue') + ' from ' + str(o_price) + ' to ' + str(n_price))
+        elif discount > 50 and discount < 75:
+            print(title, colored(discount, 'yellow') + ' from ' + str(o_price) + ' to ' + str(n_price))
+        elif discount > 75:
+            print(title, colored(discount, 'green') + ' from ' + str(o_price) + ' to ' + str(n_price))
